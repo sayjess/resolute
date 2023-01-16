8728db8a594616c0503dc422d2fa749c59db2eab
Jess <jessjelardo@gmail.com>
2023-01-16 15:18:50 +0800

    added routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Header from "./components/Header"
-import Cart from "./pages/Cart"
-import Photos from "./pages/Photos"
 
 function App() {    
     return (
-        <div>
+        <div>            
             <Header />
             <h1>Home Page</h1>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react"
+import { Link, Routes, Route } from "react-router-dom"
+import Cart from "../pages/Cart"
+import Photos from "../pages/Photos"
 
 function Header() {
     return (
         <header>
-            <h2>Pic Some</h2>
-            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            <Link to='/'><
+                h2>Pic Some</h2>
+            </Link>
+            <Link to='/cart'>
+                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            </Link>
+
+            <Routes>
+                <Route path='/' element={<Photos/>} />
+                <Route path='/cart' element={<Cart/>} />
+            </Routes>
         </header>
     )
 }
